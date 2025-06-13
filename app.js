ec11948f8fc6356d52d582930738c35aee8968cb
DexRash <54085512+DexRash@users.noreply.github.com>
2025-06-13 22:17:12 +0700

    Update app.js

--- app.js
+++ app.js
@@ -267,7 +267,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.getElementById("session-title").textContent = sessionInfo.title;
     document.getElementById("session-dates").textContent = sessionInfo.dates;
-    document.getElementById("last-updated-date").textContent = lastUpdatedDate;
+    document.getElementById("last-updated-date").innerHTML = lastUpdatedDate;
 
     history.replaceState({ page: "page-main", title: initialTitle }, initialTitle, "#page-main");
     showPage("page-main", initialTitle, false);
